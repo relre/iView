5cf9712c09e0805a230086aa26ebc05a11ce8991
relre <relreburada@gmail.com>
2024-10-24 01:26:20 +0300

    questionlistpage updated

--- frontend/src/pages/QuestionListPage.jsx
+++ frontend/src/pages/QuestionListPage.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useCallback, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 import { DndProvider, useDrag, useDrop } from 'react-dnd';
-import { PencilSquareIcon, TrashIcon } from '@heroicons/react/24/outline';
+import { PencilSquareIcon, TrashIcon, ClockIcon, QuestionMarkCircleIcon, PlusCircleIcon } from '@heroicons/react/24/outline';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import update from 'immutability-helper';
 import useQuestionPackageStore from '../store/questionPackageStore';
@@ -14,7 +14,9 @@ const QuestionListPage = () => {
   const [editQuestionId, setEditQuestionId] = useState(null);
   const [editQuestion, setEditQuestion] = useState({ text: '', minutes: 0, order: 0 });
   const [isSubmitting, setIsSubmitting] = useState(false);
-  const [showModal, setShowModal] = useState(false);
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [showAddModal, setShowAddModal] = useState(false);
+  const [showEditModal, setShowEditModal] = useState(false);
   const [questionToDelete, setQuestionToDelete] = useState(null);
 
   useEffect(() => {
@@ -39,6 +41,7 @@ const QuestionListPage = () => {
       console.error('Failed to add question:', error);
     } finally {
       setIsSubmitting(false);
+      setShowAddModal(false);
     }
   };
 
@@ -52,6 +55,8 @@ const QuestionListPage = () => {
         fetchQuestionPackages();
       } catch (error) {
         console.error('Failed to update question:', error);
+      } finally {
+        setShowEditModal(false);
       }
     }
   };
@@ -64,7 +69,7 @@ const QuestionListPage = () => {
       } catch (error) {
         console.error('Failed to delete question:', error);
       } finally {
-        setShowModal(false);
+        setShowDeleteModal(false);
         setQuestionToDelete(null);
       }
     }
@@ -73,11 +78,12 @@ const QuestionListPage = () => {
   const openEditQuestionForm = (question) => {
     setEditQuestionId(question._id);
     setEditQuestion(question);
+    setShowEditModal(true);
   };
 
   const openDeleteModal = (questionId) => {
     setQuestionToDelete(questionId);
-    setShowModal(true);
+    setShowDeleteModal(true);
   };
 
   const moveQuestion = useCallback((dragIndex, hoverIndex) => {
@@ -108,16 +114,56 @@ const QuestionListPage = () => {
 
   if (!selectedPackage) return <div>Loading...</div>;
 
+  const totalMinutes = selectedPackage.questions.reduce((sum, question) => sum + question.minutes, 0);
+
   return (
     <DndProvider backend={HTML5Backend}>
       <div>
         <h1 className="text-4xl py-4 font-bold text-rtwgreen mb-4">Questions in {selectedPackage.title}</h1>
-        <p className="mb-4">Total Questions: {selectedPackage.questions.length}</p>
+       
+        <div className='flex space-x-5'>
+          <div className="mb-4 p-4 rounded shadow w-1/5 flex items-center green-gradient">
+            <div className="mr-4 flex items-center justify-center h-10 w-10 ">
+              <QuestionMarkCircleIcon className="w-8 h-8 text-center text-rtwgreen " />
+            </div>
+            <div>
+              <div className='text-2xl font-bold'>{selectedPackage.questions.length}</div>
+              Total Questions
+            </div>
+          </div>
+
+          <div className="mb-4 p-4 rounded  shadow w-1/5 flex items-center yellow-gradient">
+            <div className="mr-4 flex items-center justify-center h-10 w-10">
+              <ClockIcon className="w-8 h-8 text-center text-rtwyellow " />
+            </div>
+            <div>
+              <div className='text-2xl font-bold'>{totalMinutes}</div>
+              Total Minutes
+            </div>
+          </div>
+        </div>
+
+        <div className='flex justify-end'>
+        <button onClick={() => setShowAddModal(true)} className="bg-rtwgreen hover:bg-rtwgreendark text-white px-4 py-2 rounded mb-4 flex items-center">
+          <PlusCircleIcon className="h-6 w-6 mr-2" /> Add New Question
+        </button>
+        </div>
+
         <table className="min-w-full border-separate border-spacing-y-2 border-spacing-x-0">
           <thead>
             <tr>
-              <th className="py-2 px-4 border-b text-start">Question Text</th>
-              <th className="py-2 px-4 border-b text-end">Minutes</th>
+              <th className="py-2 px-4 border-b text-start">
+                <div className="flex items-center justify-start space-x-1">
+                  <QuestionMarkCircleIcon className="w-6 h-6 text-black mr-1" />
+                  Questions
+                </div>
+              </th>
+              <th className="py-2 px-4 border-b text-end">
+                <div className="flex items-center justify-end space-x-1">
+                  <ClockIcon className="w-6 h-6 text-black mr-1" />
+                  Minutes
+                </div>
+              </th>
               <th className="py-2 px-4 border-b text-end">Actions</th>
             </tr>
           </thead>
@@ -125,55 +171,77 @@ const QuestionListPage = () => {
             {selectedPackage.questions.map((question, index) => renderQuestion(question, index))}
           </tbody>
         </table>
-        <form onSubmit={handleAddQuestion} className="mt-4">
-          <input
-            type="text"
-            value={newQuestion.text}
-            onChange={(e) => setNewQuestion({ ...newQuestion, text: e.target.value })}
-            placeholder="New Question Text"
-            className="w-full p-2 border border-gray-300 rounded mb-2"
-            required
-          />
-          <input
-            type="number"
-            value={newQuestion.minutes}
-            onChange={(e) => setNewQuestion({ ...newQuestion, minutes: parseInt(e.target.value) })}
-            placeholder="Minutes"
-            className="w-full p-2 border border-gray-300 rounded mb-2"
-            required
-          />
-          <button type="submit" className="bg-green-500 text-white px-4 py-2 rounded" disabled={isSubmitting}>
-            {isSubmitting ? 'Adding...' : 'Add Question'}
-          </button>
-        </form>
-        {editQuestionId && (
-          <form onSubmit={handleEditQuestion} className="mt-4">
-            <input
-              type="text"
-              value={editQuestion.text || ''}
-              onChange={(e) => setEditQuestion({ ...editQuestion, text: e.target.value })}
-              placeholder="Edit Question Text"
-              className="w-full p-2 border border-gray-300 rounded mb-2"
-              required
-            />
-            <input
-              type="number"
-              value={editQuestion.minutes || 0}
-              onChange={(e) => setEditQuestion({ ...editQuestion, minutes: parseInt(e.target.value) })}
-              placeholder="Minutes"
-              className="w-full p-2 border border-gray-300 rounded mb-2"
-              required
-            />
-            <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded">Update Question</button>
-          </form>
-        )}
-        {showModal && (
+
+        {showAddModal && (
           <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-            <div className="bg-white p-6 rounded shadow-lg">
-              <h2 className="text-xl mb-4">Are you sure you want to delete this question?</h2>
-              <div className="flex justify-end space-x-4">
-                <button onClick={() => setShowModal(false)} className="bg-gray-500 text-white px-4 py-2 rounded">No</button>
-                <button onClick={handleDeleteQuestion} className="bg-red-500 text-white px-4 py-2 rounded">Yes</button>
+            <div className="bg-white p-6 rounded-lg shadow-lg">
+              <h2 className="text-xl mb-4">Add New Question</h2>
+              <form onSubmit={handleAddQuestion}>
+                <input
+                  type="text"
+                  value={newQuestion.text}
+                  onChange={(e) => setNewQuestion({ ...newQuestion, text: e.target.value })}
+                  placeholder="New Question Text"
+                  className="w-full p-2 border border-gray-300 rounded mb-2"
+                  required
+                />
+                <input
+                  type="number"
+                  value={newQuestion.minutes}
+                  onChange={(e) => setNewQuestion({ ...newQuestion, minutes: parseInt(e.target.value) })}
+                  placeholder="Minutes"
+                  className="w-full p-2 border border-gray-300 rounded mb-2"
+                  required
+                />
+                <div className="flex justify-end space-x-4">
+                  <button onClick={() => setShowAddModal(false)} className="bg-white hover:bg-gray-100 text-sm text-black border px-4 py-2 rounded-lg">Cancel</button>
+                  <button type="submit" className="bg-rtwgreen hover:bg-rtwgreendark text-sm text-white px-4 py-2 rounded-lg" disabled={isSubmitting}>
+                    {isSubmitting ? 'Adding...' : 'Add Question'}
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        )}
+
+        {showEditModal && (
+          <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+            <div className="bg-white p-6 rounded-lg shadow-lg">
+              <h2 className="text-xl mb-4">Edit Question</h2>
+              <form onSubmit={handleEditQuestion}>
+                <input
+                  type="text"
+                  value={editQuestion.text || ''}
+                  onChange={(e) => setEditQuestion({ ...editQuestion, text: e.target.value })}
+                  placeholder="Edit Question Text"
+                  className="w-full p-2 border border-gray-300 rounded mb-2"
+                  required
+                />
+                <input
+                  type="number"
+                  value={editQuestion.minutes || 0}
+                  onChange={(e) => setEditQuestion({ ...editQuestion, minutes: parseInt(e.target.value) })}
+                  placeholder="Minutes"
+                  className="w-full p-2 border border-gray-300 rounded mb-2"
+                  required
+                />
+                <div className="flex justify-end space-x-4">
+                  <button onClick={() => setShowEditModal(false)} className="bg-white hover:bg-gray-100 text-sm text-black border px-4 py-2 rounded-lg">Cancel</button>
+                  <button type="submit" className="bg-rtwgreen hover:bg-rtwgreendark text-sm text-white px-4 py-2 rounded-lg">Update Question</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        )}
+
+        {showDeleteModal && (
+          <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+            <div className="bg-white p-6 rounded-lg shadow-lg">
+              <TrashIcon className="text-gray-400 dark:text-gray-500 w-11 h-11 mb-3.5 mx-auto" />
+              <h2 className="text-gray-500 mb-4">Are you sure you want to delete this question?</h2>
+              <div className="flex justify-center space-x-4">
+                <button onClick={() => setShowDeleteModal(false)} className="bg-white hover:bg-gray-100 text-sm text-black border px-4 py-2 rounded-lg">No, cancel</button>
+                <button onClick={handleDeleteQuestion} className="bg-red-600 hover:bg-red-700 text-sm text-white px-4 py-2 rounded-lg">Yes, I'm sure</button>
               </div>
             </div>
           </div>
@@ -223,8 +291,12 @@ const Question = ({ question, index, moveQuestion, openEditQuestionForm, openDel
 
   return (
     <tr ref={ref} className={`p-2 bg-white mb-2 rounded shadow-sm hover:bg-rtwgreenligth ${isDragging ? 'opacity-50' : ''}`}>
-      <td className="py-2 px-4 border-l border-t border-b rounded-l-lg">{question.text || 'No text available'}</td>
-      <td className="py-2 px-4 border-t border-b text-end">{question.minutes} minutes</td>
+      <td className="py-2 px-4 border-l border-t border-b rounded-l-lg">
+        {question.text || 'No text available'}
+      </td>
+      <td className="py-2 px-4 border-t border-b text-end">
+        <button className="px-2 py-1 rounded text-[0.9rem] text-yellow-500 bg-yellow-100 mr-2"><strong>{question.minutes}</strong> minutes</button>
+      </td>
       <td className="py-2 px-4 border-r border-t border-b rounded-r-lg text-end flex justify-end space-x-2">
         <button onClick={() => openEditQuestionForm(question)} className="px-2 py-1 rounded flex items-center mr-2 hover:text-rtwgreen"><PencilSquareIcon className="h-6 w-6 mr-1" />Edit</button>
         <button onClick={() => openDeleteModal(question._id)} className="px-2 py-1 rounded flex items-center mr-2 hover:text-red-500"><TrashIcon className="h-6 w-6 mr-1" />Delete</button>
