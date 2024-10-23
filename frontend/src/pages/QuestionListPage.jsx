fc4f570a6f21ef188a9e69fe674e112e6c30689b
relre <relreburada@gmail.com>
2024-10-23 13:37:35 +0300

    update ui

--- frontend/src/pages/QuestionListPage.jsx
+++ frontend/src/pages/QuestionListPage.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, useCallback, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 import { DndProvider, useDrag, useDrop } from 'react-dnd';
+import { PencilSquareIcon, TrashIcon } from '@heroicons/react/24/outline';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import update from 'immutability-helper';
 import useQuestionPackageStore from '../store/questionPackageStore';
@@ -13,6 +14,8 @@ const QuestionListPage = () => {
   const [editQuestionId, setEditQuestionId] = useState(null);
   const [editQuestion, setEditQuestion] = useState({ text: '', minutes: 0, order: 0 });
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [questionToDelete, setQuestionToDelete] = useState(null);
 
   useEffect(() => {
     fetchQuestionPackages();
@@ -25,17 +28,17 @@ const QuestionListPage = () => {
 
   const handleAddQuestion = async (e) => {
     e.preventDefault();
-    if (isSubmitting) return; // Eğer zaten submit ediliyorsa, işlemi durdur
-    setIsSubmitting(true); // Butonu devre dışı bırak
+    if (isSubmitting) return;
+    setIsSubmitting(true);
     try {
       const order = selectedPackage.questions.length;
       await addQuestion(selectedPackage._id, { ...newQuestion, order });
       setNewQuestion({ text: '', minutes: 0, order: 0 });
-      fetchQuestionPackages(); // Yeni veriyi almak için paketleri yeniden yükle
+      fetchQuestionPackages();
     } catch (error) {
       console.error('Failed to add question:', error);
     } finally {
-      setIsSubmitting(false); // Butonu tekrar etkinleştir
+      setIsSubmitting(false);
     }
   };
 
@@ -46,20 +49,23 @@ const QuestionListPage = () => {
         await updateQuestion(selectedPackage._id, editQuestionId, editQuestion);
         setEditQuestionId(null);
         setEditQuestion({ text: '', minutes: 0, order: 0 });
-        fetchQuestionPackages(); // Yeni veriyi almak için paketleri yeniden yükle
+        fetchQuestionPackages();
       } catch (error) {
         console.error('Failed to update question:', error);
       }
     }
   };
 
-  const handleDeleteQuestion = async (questionId) => {
-    if (window.confirm('Are you sure you want to delete this question?')) {
+  const handleDeleteQuestion = async () => {
+    if (questionToDelete) {
       try {
-        await deleteQuestion(selectedPackage._id, questionId);
-        fetchQuestionPackages(); // Yeni veriyi almak için paketleri yeniden yükle
+        await deleteQuestion(selectedPackage._id, questionToDelete);
+        fetchQuestionPackages();
       } catch (error) {
         console.error('Failed to delete question:', error);
+      } finally {
+        setShowModal(false);
+        setQuestionToDelete(null);
       }
     }
   };
@@ -67,6 +73,11 @@ const QuestionListPage = () => {
   const openEditQuestionForm = (question) => {
     setEditQuestionId(question._id);
     setEditQuestion(question);
+  };
+
+  const openDeleteModal = (questionId) => {
+    setQuestionToDelete(questionId);
+    setShowModal(true);
   };
 
   const moveQuestion = useCallback((dragIndex, hoverIndex) => {
@@ -90,7 +101,7 @@ const QuestionListPage = () => {
         question={question}
         moveQuestion={moveQuestion}
         openEditQuestionForm={openEditQuestionForm}
-        handleDeleteQuestion={handleDeleteQuestion}
+        openDeleteModal={openDeleteModal}
       />
     );
   };
@@ -100,11 +111,20 @@ const QuestionListPage = () => {
   return (
     <DndProvider backend={HTML5Backend}>
       <div>
-        <h1 className="text-2xl font-bold mb-4">Questions in {selectedPackage.title}</h1>
+        <h1 className="text-4xl py-4 font-bold text-rtwgreen mb-4">Questions in {selectedPackage.title}</h1>
         <p className="mb-4">Total Questions: {selectedPackage.questions.length}</p>
-        <ul>
-          {selectedPackage.questions.map((question, index) => renderQuestion(question, index))}
-        </ul>
+        <table className="min-w-full border-separate border-spacing-y-2 border-spacing-x-0">
+          <thead>
+            <tr>
+              <th className="py-2 px-4 border-b text-start">Question Text</th>
+              <th className="py-2 px-4 border-b text-end">Minutes</th>
+              <th className="py-2 px-4 border-b text-end">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {selectedPackage.questions.map((question, index) => renderQuestion(question, index))}
+          </tbody>
+        </table>
         <form onSubmit={handleAddQuestion} className="mt-4">
           <input
             type="text"
@@ -147,12 +167,23 @@ const QuestionListPage = () => {
             <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded">Update Question</button>
           </form>
         )}
+        {showModal && (
+          <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+            <div className="bg-white p-6 rounded shadow-lg">
+              <h2 className="text-xl mb-4">Are you sure you want to delete this question?</h2>
+              <div className="flex justify-end space-x-4">
+                <button onClick={() => setShowModal(false)} className="bg-gray-500 text-white px-4 py-2 rounded">No</button>
+                <button onClick={handleDeleteQuestion} className="bg-red-500 text-white px-4 py-2 rounded">Yes</button>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </DndProvider>
   );
 };
 
-const Question = ({ question, index, moveQuestion, openEditQuestionForm, handleDeleteQuestion }) => {
+const Question = ({ question, index, moveQuestion, openEditQuestionForm, openDeleteModal }) => {
   const ref = useRef(null);
   const [, drop] = useDrop({
     accept: 'QUESTION',
@@ -191,15 +222,14 @@ const Question = ({ question, index, moveQuestion, openEditQuestionForm, handleD
   drag(drop(ref));
 
   return (
-    <li ref={ref} className={`p-2 bg-white mb-2 rounded shadow ${isDragging ? 'opacity-50' : ''}`}>
-      <div className="flex justify-between items-center">
-        <span>{question.text || 'No text available'} - {question.minutes} minutes</span>
-        <div>
-          <button onClick={() => openEditQuestionForm(question)} className="bg-blue-500 text-white px-2 py-1 rounded mr-2">Edit</button>
-          <button onClick={() => handleDeleteQuestion(question._id)} className="bg-red-500 text-white px-2 py-1 rounded">Delete</button>
-        </div>
-      </div>
-    </li>
+    <tr ref={ref} className={`p-2 bg-white mb-2 rounded shadow-sm hover:bg-rtwgreenligth ${isDragging ? 'opacity-50' : ''}`}>
+      <td className="py-2 px-4 border-l border-t border-b rounded-l-lg">{question.text || 'No text available'}</td>
+      <td className="py-2 px-4 border-t border-b text-end">{question.minutes} minutes</td>
+      <td className="py-2 px-4 border-r border-t border-b rounded-r-lg text-end flex justify-end space-x-2">
+        <button onClick={() => openEditQuestionForm(question)} className="px-2 py-1 rounded flex items-center mr-2 hover:text-rtwgreen"><PencilSquareIcon className="h-6 w-6 mr-1" />Edit</button>
+        <button onClick={() => openDeleteModal(question._id)} className="px-2 py-1 rounded flex items-center mr-2 hover:text-red-500"><TrashIcon className="h-6 w-6 mr-1" />Delete</button>
+      </td>
+    </tr>
   );
 };
 
