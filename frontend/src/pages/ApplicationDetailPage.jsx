55979002cf40f08099e081c41e3f86db7789b1ca
Okan Patır <relre@outlook.com>
2024-10-29 16:35:01 +0300

    Update ApplicationDetailPage.jsx

--- frontend/src/pages/ApplicationDetailPage.jsx
+++ frontend/src/pages/ApplicationDetailPage.jsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import useInterviewStore from '../store/interviewStore';
+import { PlayCircleIcon } from '@heroicons/react/24/outline';
 
 const ApplicationDetailPage = () => {
   const { id, applicationId } = useParams();
   const application = useInterviewStore((state) => state.application);
   const fetchApplicationById = useInterviewStore((state) => state.fetchApplicationById);
+  const updateApplicationStatus = useInterviewStore((state) => state.updateApplicationStatus);
+
   const fetchInterviewQuestions = useInterviewStore((state) => state.fetchInterviewQuestions);
   const interviewQuestions = useInterviewStore((state) => state.interviewQuestions); // interviewQuestions state'ini al
   const [status, setStatus] = useState('');
@@ -54,41 +57,74 @@ const ApplicationDetailPage = () => {
     currentTime += question.minutes * 60; // Dakikayı saniyeye çevir
     return (
       <li key={index}>
-        {formatTime(startTime)} - {formatTime(currentTime)} - {question.text}
+        <span className="bg-gray-200 text-gray-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded">
+        {formatTime(startTime)} - {formatTime(currentTime)}  </span>
+        <span className='text-sm'> {question.text}</span>
       </li>
     );
   }) : <li>No questions available</li>;
 
   return (
     <div>
-      <h1 className="text-2xl font-bold mb-4">Application Detail</h1>
-      <p><strong>Name:</strong> {application.name}</p>
-      <p><strong>Surname:</strong> {application.surname}</p>
-      <p><strong>Email:</strong> {application.email}</p>
-      <p><strong>Phone:</strong> {application.phone}</p>
-      <p><strong>Status:</strong> {application.status}</p>
+            <h1 className="text-4xl py-4 font-bold text-rtwgreen mb-4">Application Detail</h1>
 
-      <video width="320" height="240" controls>
+    
+
+      <div className='flex p-6 rounded-lg bg-white shadow-lg'>
+        <div className='w-1/2 p-3'>
+      <video className='w-full rounded-lg' controls>
         <source src={videoUrl}/>
         Your browser does not support the video tag.
       </video>
+      </div>
+      <div className='w-1/2 p-3'>
+      <div>
+      <h2 className="text-xl font-bold mb-2 text-rtwgreen border-b w-1/3 border-rtwgreen">Application Detail</h2>
+      <div className='flex items-center'><PlayCircleIcon className='w-6 h-6 text-rtwgreen mr-2' /> 
+              <p className='text-xl'><strong>{application.name}  {' '}
+              <span className='text-rtwgreendark'>{application.surname}</span></strong> </p></div>
+              <p className=' mt-2'><strong>Email:</strong> {application.email}</p>
+              <p className=' mb-2'><strong>Phone:</strong> {application.phone}</p>
+              <p>
+              {application.status === 'approved' ? (
+                  <span className="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded">
+                    Approved
+                  </span>
+                ) : application.status === 'rejected' ? (
+                  <span className="bg-red-100 text-red-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded">
+                    Rejected</span>
 
+                ) : (
+                  <span class="bg-yellow-100 text-yellow-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded">Pending</span>
+            
+                )}
+                              
+               </p>
+
+      </div>
+      <div className='overflow-y-auto max-h-96 mt-4'>
+      <h2 className="text-xl font-bold mb-2 text-rtwgreen border-b w-1/3 border-rtwgreen">Questions</h2>
+      <ul>
+        {questionList}
+      </ul>
+      </div>
+      </div>
+
+      </div>
+      <div className='flex justify-center mt-5 mr-10'>
       <form onSubmit={handleSubmit}>
-        <label>
+        <label className='mb-2  text-sm font-medium text-gray-900'>
           Update Status:
-          <select value={status} onChange={handleStatusChange}>
+          <select className='ml-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:border-rtwgreen p-2.5 ' value={status} onChange={handleStatusChange}>
             <option value="pending">Pending</option>
             <option value="approved">Approved</option>
             <option value="rejected">Rejected</option>
           </select>
         </label>
-        <button type="submit">Update</button>
+        <button type="submit" className='bg-rtwgreen hover:bg-rtwgreendark text-sm text-white px-4 py-2 ml-2 rounded-lg'>Update</button>
       </form>
-
-      <h2 className="text-xl font-bold mt-4">Questions</h2>
-      <ul>
-        {questionList}
-      </ul>
+      </div>
+     
     </div>
   );
 };
