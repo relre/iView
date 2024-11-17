dbfa530a1790f95046509c1500afe0e210fa2df0
Okan Patır <relre@outlook.com>
2024-11-17 20:36:31 +0300

    update proxy

--- backend/src/proxy.ts
+++ backend/src/proxy.ts
@@ -1,14 +1,16 @@
 import express from 'express';
-import request from 'request';
+import { createProxyServer } from 'http-proxy';
 
 const app = express();
+const proxy = createProxyServer();
 
 app.get('/proxy', (req, res) => {
   const videoUrl = req.query.url as string;
   if (!videoUrl) {
     return res.status(400).json({ error: 'URL is required' });
   }
-  request(videoUrl).pipe(res).on('error', (err) => {
+
+  proxy.web(req, res, { target: videoUrl, changeOrigin: true }, (err) => {
     console.error('Error fetching video:', err.message);
     res.status(500).json({ error: 'Failed to fetch video', details: err.message });
   });
