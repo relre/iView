b95fb3624589fe84a47d605a57b297bbeecdd6a2
Okan Patır <relre@outlook.com>
2024-11-17 18:56:51 +0300

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -23,11 +23,11 @@ app.use(cors({
 }));
 app.use(express.json());
 
-
+console.log(process.env.MONGO_URI);
 mongoose.connect(process.env.MONGO_URI!, {})
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
-
+  console.log(process.env.MONGO_URI);
 app.use('/api', questionPackageRoutes);
 app.use('/api', interviewRoutes);
 app.use('/api/auth', authRoutes);
