2a7fdf999cc82d7854c037081491c4d477d68bd8
Okan Patır <relre@outlook.com>
2024-11-17 20:27:24 +0300

    proxy update

--- backend/src/proxy.ts
+++ backend/src/proxy.ts
@@ -1,17 +1,22 @@
 import express from 'express';
-import request from 'request';
+import { createProxyMiddleware, Options } from 'http-proxy-middleware';
 
 const app = express();
 
-app.get('/proxy', (req, res) => {
+app.use('/proxy', (req, res, next) => {
   const videoUrl = req.query.url as string;
   if (!videoUrl) {
     return res.status(400).json({ error: 'URL is required' });
   }
-request(videoUrl).pipe(res).on('error', (err: Error) => {
-    console.error('Error fetching video:', err.message);
-    res.status(500).json({ error: 'Failed to fetch video', details: err.message });
-});
+  const proxyOptions: Options = {
+    target: videoUrl,
+    changeOrigin: true,
+    onError: (err: Error, _req: express.Request, res: express.Response) => {
+      console.error('Error fetching video:', err.message);
+      res.status(500).json({ error: 'Failed to fetch video', details: err.message });
+    }
+  };
+  createProxyMiddleware(proxyOptions)(req, res, next);
 });
 
 export default app;
