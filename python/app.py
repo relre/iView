fc81984535c88a0eb23111f7aec1c606079ea3a2
Okan Patır <relre@outlook.com>
2024-11-17 16:14:12 +0300

    Update app.py

--- python/app.py
+++ python/app.py
@@ -77,4 +77,4 @@ def transcribe():
     return jsonify({"datax": datax})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000, ssl_context=('path/to/cert.pem', 'path/to/key.pem'))
+    app.run(debug=True, host='0.0.0.0', port=5000, ssl_context=('/etc/letsencrypt/live/iviewback.relre.dev/fullchain.pem', '/etc/letsencrypt/live/iviewback.relre.dev/privkey.pem'))
